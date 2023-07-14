f408d6fbf578c304f90344578937b2f4070214ab
moveold0611 <b44sup97045@gmail.com>
2023-07-14 14:16:58 +0900

    feat : 최초 업로드

---
+++ socket_project_client/src/socket_project_client/Client.java
@@ -0,0 +1,7 @@
+package socket_project_client;
+
+import java.net.Socket;
+
+public class Client {
+	private Socket socket;
+}
---
+++ socket_project_server/src/socket_project_server/Server.java
@@ -0,0 +1,7 @@
+package socket_project_server;
+
+import java.net.Socket;
+
+public class Server {
+	public static Socket socket;
+}
