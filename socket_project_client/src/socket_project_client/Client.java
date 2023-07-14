1a6257584367ab8899b8b9228e3f22983b34b503
moveold0611 <b44sup97045@gmail.com>
2023-07-14 14:22:45 +0900

    feat: 최초 업로드_2

--- socket_project_client/src/socket_project_client/Client.java
+++ socket_project_client/src/socket_project_client/Client.java
@@ -1,7 +1,42 @@
 package socket_project_client;
 
+import java.awt.EventQueue;
 import java.net.Socket;
 
-public class Client {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Client extends JFrame {
 	private Socket socket;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Client frame = new Client();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Client() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
 }
